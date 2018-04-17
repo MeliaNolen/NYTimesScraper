27ef39bbbd726e3c9adf4c920c370158c2898b9e
MeliaNolen <melianolen@wifi_stu-10-38-75-239.uncc.edu>
2018-04-17 14:28:40 -0400

    added nytimes scraper

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ var bodyParser = require("body-parser");
 var cheerio = require('cheerio');
 var request = require('request');
 var logger = require("morgan");
+
+// scraping data
+request("https://www.nytimes.com", function(error, response, html) {
+  var $ = cheerio.load(html);
+  var results = [];
+
+  $("h2.story-heading > a").each(function(i, element) {
+    var title = $(element).text().trim();
+    var link = $(element).attr("href");
+
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+  console.log(results);
+});
