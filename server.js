b0c5ea79342283421c58c8f7d73d4fed92621f7c
MeliaNolen <melianolen@wifi_stu-10-38-75-239.uncc.edu>
2018-04-17 14:17:03 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var exphb = require('express-handlebars');
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require('cheerio');
+var request = require('request');
+var logger = require("morgan");
